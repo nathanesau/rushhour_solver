af57f9f2b56c41b257077ebfa9fb74a7350872ee
Nathan Esau <nathan.esau@nielseniq.com>
2021-03-13 21:06:08 -0500

    add project cases

--- src/python/test_solve.py
+++ src/python/test_solve.py
@@ -45,10 +45,68 @@ jams = {
 }
 
 
-def test_runtime():
+project = {
+    "A00": "AA...O\n.....O\nXX...O\n...QQQ\n....CC\n..RRR.",
+    "A01": "AA...O\nP..Q.O\nPXXQ.O\nP..Q..\nB...CC\nB.RRR.",
+    "A02": "A..OOO\nA..B.P\nXX.BCP\nQQQ.CP\n..D.EE\nFFDGG.",
+    "A03": "......\n......\n.XXO..\n.AAO.P\n.B.O.P\n.BCC.P",
+    "A04": "O..P..\nO..P..\nOXXP..\n..AQQQ\n..A..B\n..RRRB",
+    "A05": "AA.O.B\nP..OQB\nPXXOQG\nPRRRQG\nD...EE\nD...FF",
+    "A06": "AA.B..\nCC.BOP\n.XXQOP\nDDEQOP\nF.EQ..\nF..RRR",
+    "A07": ".ABBCD\n.A.ECD\n.XXE.F\n..II.F\n...H..\n...H..",
+    "A08": "...AAO\n..BBCO\nXXDECO\nFFDEGG\nHHIPPP\nKKIQQQ",
+    "A09": ".ABBCC\n.A.DEE\nXX.DOF\nPQQQOF\nP.G.OH\nP.G..H",
+    "A10": "AAB.CC\nDDB..O\nPXX..O\nPQQQ.O\nP..EFF\nGG.EHH",
+    "B11": "OAAP..\nO..P..\nOXXP..\n..BQQQ\n..B..E\n..RRRE",
+    "B12": "ABB..O\nA.P..O\nXXP..O\n..PQQQ\n....C.\nRRR.C.",
+    "B13": "AABBC.\n..D.CO\n.EDXXO\nPE.FFO\nP..GHH\nPIIGKK",
+    "B14": "AAB...\n..BCC.\nDEXXFG\nDEHHFG\n..IJJ.\nKKI...",
+    "B15": ".AABB.\nCCDDOP\nQRXXOP\nQREFOP\nQREFGG\n.HHII.",
+    "B16": "AABBCO\nD.EECO\nDFPXXO\n.FPQQQ\n..P...\nGG....",
+    "B17": "AOOO..\nA.BBCC\nXXD...\nEEDP..\nQQQPFG\nRRRPFG",
+    "B18": "AABO..\nCCBO..\nPXXO..\nPQQQ..\nPDD...\nRRR...",
+    "B19": "..ABB.\n..A.J.\n.DXXJ.\n.DEEF.\n.OOOF.\n......",
+    "B20": "A..OOO\nABBC..\nXXDC.P\n..D..P\n..EFFP\n..EQQQ",
+    "C21": "AABO..\nP.BO..\nPXXO..\nPQQQ..\n......\n...RRR",
+    "C22": "..AOOO\nB.APCC\nBXXP..\n.D.PEE\nFDGG.H\nFQQQ.H",
+    "C23": "..OOOP\n..ABBP\n..AXXP\n..CDEE\n..CDFF\n..QQQ.",
+    "C24": "..ABB.\n.CA...\nDCXXE.\nDFF.E.\nOOO.G.\nCC..G.",
+    "C25": "AAB.CC\nDDB..O\nPXX.EO\nPQQQEO\nPF.GHH\n.F.GII",
+    "C26": ".A.OOO\nBA.CP.\nBXXCPD\nERRRPD\nE.F..G\n..FHHG",
+    "C27": "ABBO..\nACCO..\nXXDO.P\n..DEEP\n..F..P\n..FRRR",
+    "C28": "OOO.P.\n..A.P.\nXXA.PB\nCDDEEB\nCFFG.H\nRRRG.H",
+    "C29": "O.APPP\nO.AB..\nOXXB..\nCCDD.Q\n.....Q\nEEFF.Q",
+    "D30": "AA.OOO\n...BCC\nDXXB.P\nD.QEEP\nFFQ..P\n..QRRR",
+    "D31": "AAOBCC\n..OB..\nXXO...\nDEEFFP\nD..K.P\nHH.K.P",
+    "D32": ".AR.BB\n.AR...\nXXR...\nIDDEEP\nIFFGHP\nQQQGHP",
+    "D33": "A..RRR\nA..B.P\nXX.BCP\nQQQDCP\n..EDFF\nIIEHH.",
+    "D34": "..OAAP\n..OB.P\nXXOB.P\nKQQQ..\nKDDEF.\nGG.EF.",
+    "D35": "OPPPAA\nOBCC.Q\nOBXX.Q\nRRRD.Q\n..EDFF\nGGE..."
+}
+
+
+def test_jams_runtime():
     print("\n")
     for k, jam in jams.items():
         grid = [list(line) for line in jam.splitlines()]
+        cl = set([t for r in grid for t in r if t != '.'])
+        tl = dict((c, [(i, j) for i in range(6) for j in range(6)
+                       if grid[i][j] == c]) for c in cl)
+        v = dict((car, tl[car][0][0] != tl[car][1][0]) for car in cl)
+        src = dict((car, tl[car][0][0] if v[car] else tl[car][0][1])
+                   for car in cl)
+        metadata = Metadata(grid)
+        start = datetime.now()
+        path = solve(src, metadata)
+        end = datetime.now()
+        print((f"solved {k} in {(end-start).microseconds/1e6} seconds "
+               f"(node_count = {metadata.node_count})"))
+
+
+def test_project_runtime():
+    print("\n")
+    for k, jam in project.items():
+        grid = [list(line.lower()) for line in jam.splitlines()]
         cl = set([t for r in grid for t in r if t != '.'])
         tl = dict((c, [(i, j) for i in range(6) for j in range(6)
                        if grid[i][j] == c]) for c in cl)
